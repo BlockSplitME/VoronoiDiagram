b41f3fbc7924ec743d04ae49a7128a74d0b77be4
BlockSplitME <evgeniy.belov.12@mail.ru>
2023-10-25 13:22:20 +0300

    update

---
+++ src/frontend/src/js/requests.js
@@ -0,0 +1,28 @@
+async function initField(count, width, height) {
+    let params = new URLSearchParams(); 
+    params.set('count', count);
+    params.set('width', width);
+    params.set('height', height);
+
+    return baseRequest("initField?" + params)
+}
+async function doStep() {
+    return baseRequest("nextStep?")
+}
+async function getResult() {
+    return baseRequest("result?")
+}
+async function baseRequest(params) {
+    return fetch("http://localhost:8080/api/" + params, {
+       method: 'GET',
+    }).then(function(response){
+        console.log("Data received");
+        return response.json();
+    })
+}
+
+export default { 
+    initField,
+    doStep,
+    getResult
+}
